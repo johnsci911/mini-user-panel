b6b9a8bddc7d84c3ed3d4e814a9ff473e7400805
johnsci911 <johnkarlo.315@gmail.com>
2025-05-17 13:03:30 +0800

    Install Nativewind v4

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex flex-col flex-1 bg-slate-900 y-4">
+      <Text className="text-2xl text-center font-bold text-white">User Panel</Text>
+
+      <View className="flex-row mt-4 bg-slate-800">
+        <Text className="text-slate-100 ml-4 my-3 text-lg">Role:</Text>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
