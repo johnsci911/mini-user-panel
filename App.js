297125bb24e9e4ee25bcf0c23d8e4d8413b84c64
johnsci911 <johnkarlo.315@gmail.com>
2025-05-17 13:13:00 +0800

    Role filters

--- App.js
+++ App.js
@@ -8,7 +8,29 @@ export default function App() {
       <Text className="text-2xl text-center font-bold text-white">User Panel</Text>
 
       <View className="flex-row mt-4 bg-slate-800">
-        <Text className="text-slate-100 ml-4 my-3 text-lg">Role:</Text>
+        <View>
+          <Text className="text-slate-100 ml-4 my-auto text-xl font-bold">Role:</Text>
+        </View>
+
+        <View className="flex flex-row m-4">
+          <TouchableOpacity
+            className="bg-slate-700 py-2 px-4 rounded-md border border-slate-100"
+          >
+            <Text className="text-slate-50">All</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="ml-4 bg-slate-700 py-2 px-4 rounded-md border border-slate-100"
+          >
+            <Text className="text-slate-50">Admin</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="ml-4 bg-slate-700 py-2 px-4 rounded-md border border-slate-100"
+          >
+            <Text className="text-slate-50">User</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
